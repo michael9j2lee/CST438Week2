62ff56524382edad67456e2de8b0b7e4db49a0b5
michael lee <michaelee@csumb.edu>
2020-07-03 05:07:00 -0700

    Changed CityInfo to include the variable names required for the api. - id, name, countryCode, countryName, district, population, temp, time Changed weather_show to grab from CityInfo correctly.

--- cst438week2_assignment2/src/main/java/cst438_assignment2/domain/CityInfo.java
+++ cst438week2_assignment2/src/main/java/cst438_assignment2/domain/CityInfo.java
@@ -8,34 +8,88 @@ import javax.persistence.Transient;
 
 public class CityInfo {
 	
-
-	@Transient
-	City city;
-	// Transient marks extra field in an Entity class
-	//  that is not read/written to the database.
-	@Transient
-	TempAndTime tempAndTime;
+	int id;
+	String name;
+	String countryCode;
+	String countryName;
+	String district;
+	int population;
+	double temp;
+	long time;
 
 	public CityInfo(City city, TempAndTime tempAndTime) {
-		this.city = city;
-		this.tempAndTime = tempAndTime;
-
+		this.id = city.getID();
+		this.name = city.getName();
+		this.countryCode = city.getCountry().getCode();
+		this.countryName = city.getCountry().getName();
+		this.district = city.getDistrict();
+		this.population = city.getPopulation();
+		this.temp = tempAndTime.getTemp();
+		this.time = tempAndTime.getTime();
 	}
 
-	public City getCity() {
-		return city;
+	public int getId() {
+		return id;
 	}
 
-	public void setCity(City city) {
-		this.city = city;
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	public TempAndTime getTempAndTime() {
-		return tempAndTime;
+	public String getName() {
+		return name;
 	}
 
-	public void setTempAndTime(TempAndTime tempAndTime) {
-		this.tempAndTime = tempAndTime;
+	public void setName(String name) {
+		this.name = name;
 	}
 
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	public double getTemp() {
+		return temp;
+	}
+
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+	
 }
