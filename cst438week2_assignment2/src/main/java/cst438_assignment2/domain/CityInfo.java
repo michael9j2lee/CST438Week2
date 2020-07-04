54fed0e4265ce7c1b5473bc3d7ea0f3ef3fd4ddc
michael lee <michaelee@csumb.edu>
2020-07-03 17:49:20 -0700

    Modified CityInfo with default constructor and variable constructors.
    Added new CityRestController with Test1 working
    Added empty cityServiceTest.java
    
    TODO:
    Test2 and Test3 for CityRest controllers/CityRestControllerTest.java
    Test(s) for cityServiceTest

--- cst438week2_assignment2/src/main/java/cst438_assignment2/domain/CityInfo.java
+++ cst438week2_assignment2/src/main/java/cst438_assignment2/domain/CityInfo.java
@@ -1,12 +1,6 @@
 package cst438_assignment2.domain;
 
 import java.util.Date;
-import java.util.TimeZone;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -21,7 +15,9 @@ public class CityInfo {
 	int population;
 	double temp;
 	String time;
-
+	
+	public CityInfo() {}
+	
 	public CityInfo(City city, TempAndTime tempAndTime) {
 		this.id = city.getID();
 		this.name = city.getName();
@@ -33,9 +29,24 @@ public class CityInfo {
 		this.time = convertTime(tempAndTime);
 	}
 	
+	public CityInfo(int id, String name, String countryCode, String countryName, String district, int population,
+			double temp, String time) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.countryCode = countryCode;
+		this.countryName = countryName;
+		this.district = district;
+		this.population = population;
+		this.temp = temp;
+		this.time = time;
+	}
+
+
+
 	public String convertTime(TempAndTime t) {
 
-		Date date = new Date((long)(t.getTime()+t.getTimezone() )*1000);
+		Date date = new Date((long)(t.getTime() - t.getTimezone()  )*1000);
 		DateFormat format = new SimpleDateFormat("hh:mm a");
 		return format.format(date);
 	}
@@ -103,5 +114,58 @@ public class CityInfo {
 	public void setTime(String time) {
 		this.time = time;
 	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CityInfo other = (CityInfo) obj;
+		if (countryCode == null) {
+			if (other.countryCode != null)
+				return false;
+		} else if (!countryCode.equals(other.countryCode))
+			return false;
+		if (countryName == null) {
+			if (other.countryName != null)
+				return false;
+		} else if (!countryName.equals(other.countryName))
+			return false;
+		if (district == null) {
+			if (other.district != null)
+				return false;
+		} else if (!district.equals(other.district))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (population != other.population)
+			return false;
+		if (Double.doubleToLongBits(temp) != Double.doubleToLongBits(other.temp))
+			return false;
+		if (time == null) {
+			if (other.time != null)
+				return false;
+		} else if (!time.equals(other.time))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CityInfo [id=" + id + ", name=" + name + ", countryCode=" + countryCode + ", countryName=" + countryName
+				+ ", district=" + district + ", population=" + population + ", temp=" + temp + ", time=" + time + "]";
+	}
+	
+	
 	
 }
--- cst438week2_assignment2/src/test/java/cst438_assignment2/Cst438week2Assignment2ApplicationTests.java
+++ cst438week2_assignment2/src/test/java/cst438_assignment2/Cst438week2Assignment2ApplicationTests.java
@@ -3,6 +3,16 @@ package cst438_assignment2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+/*
+ * Part 3 – write unit tests for CityService and CityRestController classes.
+•	write a test CityServiceTest that test the city service class with MOCKS for the database repositories and weather service.
+•	write a test  CityRestControllerTest for your Rest Controller using MOCKS for the CityService class.
+•	Your tests  should test both valid and invalid city name.  There are some city names with multiple cities; such as Los Angeles.   Make sure your Rest Controller has tests for  all 3 situations.
+
+ * 
+ * 
+ */
 @SpringBootTest
 class Cst438week2Assignment2ApplicationTests {
 
